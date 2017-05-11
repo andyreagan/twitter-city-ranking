806f1654d05744f05f339e5331f33c428c470cca
Andy Reagan <github@andyreagan.com>
2017-05-10 20:49:27 -0400

    updates sitting around as of now

--- geofunctions.py
+++ geofunctions.py
@@ -1,1 +1,120 @@
-fiona-shapely-speedtest.py
+# load all of the geometries
+import fiona
+from shapely.geometry import Point,Polygon,MultiPolygon
+from shapely import speedups
+speedups.enable()
+
+# we'll use most of these
+from json import loads
+import codecs
+import datetime
+# import re
+# import numpy
+# from labMTsimple.storyLab import *
+import sys
+
+def loadCities():
+    c = fiona.open('shapefiles/cb_2013_us_ua10_500k.shp','r')
+    polygonList = []
+    nameList = []
+    polygonCount = 0
+    multiPolygonCount = 0
+    for city in list(c):
+        nameList.append(city['properties']['NAME10'])
+        if city['geometry']['type'] == 'Polygon':
+            polygonCount += 1
+            coordinates = city['geometry']['coordinates']
+            polygonList.append(Polygon(coordinates[0]))
+        elif city['geometry']['type'] == 'MultiPolygon':
+            multiPolygonCount += 1
+            coordinates = city['geometry']['coordinates']
+            coordinates_w_holes = [(tuple(c[0]),()) if len(c) == 1 else (tuple(c[0]),(c[1:])) for c in coordinates]
+            polygonList.append(MultiPolygon(coordinates_w_holes))
+        else:
+            raise('unknown geometry ' % city['geometry']['type'])
+    print('done loading')
+    return polygonList,nameList
+
+def loadGeneric(shapefile,key="NAME10"):
+    """Load any shapefile.
+
+    Ex usage:
+    park_polygon_list,park_name_list = loadGeneric("shapefiles/nps_boundary.shp",key="UNIT_NAME"):"""
+    c = fiona.open(shapefile,'r')
+    polygonList = []
+    nameList = []
+    polygonCount = 0
+    multiPolygonCount = 0
+    for city in list(c):
+        nameList.append(city['properties'][key])
+        if city['geometry']['type'] == 'Polygon':
+            polygonCount += 1
+            coordinates = city['geometry']['coordinates']
+            polygonList.append(Polygon(coordinates[0]))
+        elif city['geometry']['type'] == 'MultiPolygon':
+            multiPolygonCount += 1
+            coordinates = city['geometry']['coordinates']
+            coordinates_w_holes = [(tuple(c[0]),()) if len(c) == 1 else (tuple(c[0]),(c[1:])) for c in coordinates]
+            polygonList.append(MultiPolygon(coordinates_w_holes))
+        else:
+            raise('unknown geometry ' % city['geometry']['type'])
+    print('done loading')
+    return polygonList,nameList
+
+def cityID(polygonList,pt):
+    for i,city in enumerate(polygonList):
+        if city.contains(pt):
+            return i
+    return -1
+
+def gzipper(polygonList,nameList,outfile,folder="citytweets"):
+    lineCount = 0
+    tweetCount = 0
+    geoCount = 0
+    geoLocatedCount = 0
+    f = sys.stdin
+    for line in f:
+        lineCount += 1
+        try:
+            tweet = loads(line)
+            tweetCount +=1 
+        except:
+            print("failed to load a tweet")
+        geo_tweet_bool = False
+        myCityCoords = []
+        try:
+            if tweet['coordinates']:
+                myCityCoords = tweet['coordinates']['coordinates']
+                geo_tweet_bool = True
+            elif tweet['geo']:
+                myCityCoords = tweet['geo']['coordinates']
+                geo_tweet_bool = True
+        except:
+            pass
+        if geo_tweet_bool:
+            geoCount += 1
+            myPt = Point(myCityCoords)
+            myCityID = cityID(polygonList,myPt)
+            if myCityID > -1:
+                # print(nameList[myCityID])
+                geoLocatedCount += 1
+                f = codecs.open('{0}/{1}/{2}.txt'.format(folder,myCityID,outfile),'a','utf8')
+                f.write("{0}\t{1}\t{2}\t".format(tweet['user']['id'],myCityCoords[0],myCityCoords[1]))
+                # tweettext = unicode(tweet['text'])
+                # f.write(tweet['text']) # .replace('\n',' ').replace('\t',' '))
+                f.write(tweet['text'].replace('\n',' ').replace('\t',' '))
+                f.write("\n")
+                f.close()
+            else:
+                pass
+    print('read {0} lines, {1} tweets, and classified {3} of {2} geotweets'.format(lineCount,tweetCount,geoCount,geoLocatedCount))
+
+if __name__ == '__main__':
+    date = datetime.datetime.strptime(sys.argv[1],'%Y-%m-%d')
+    # polygonList,nameList = loadCities()
+    # polygonList,nameList = loadCities()
+    polygonList,nameList = loadGeneric("shapefiles/nps_boundary.shp",key="UNIT_NAME")
+    gzipper(polygonList,nameList,date.strftime('%Y-%m-%d'),folder="parktweets")
+    
+
+
--- qsub.py
+++ qsub.py
@@ -8,11 +8,14 @@ import subprocess
 import time
 
 jobs = int(subprocess.check_output("showq | grep areagan | wc -l",shell=True))
-print jobs
+print(jobs)
 
-if jobs < 100:
-    ctime = subprocess.check_output("date +%S.%M.%H.%d.%m.%y",shell=True).rstrip()
+max_jobs = 150
+jobs_remaining = max_jobs - jobs
 
+loop_counter = 0
+
+while jobs_remaining > 24:
     f = open('currdate.txt','r')
     tmp = f.read().rstrip()
     f.close()
@@ -21,36 +24,43 @@ if jobs < 100:
     date += datetime.timedelta(days=1)
 
     # if date > datetime.datetime(2014,07,14):
-    # if date > datetime.datetime(2013,5,27):
-    if date > datetime.datetime.now():
+    # if date > datetime.datetime(2014,12,31):
+    if date > datetime.datetime(2013,12,31):
+    # if date > datetime.datetime.now():
         print('date past search range')
+        break
     else:
+        loop_counter += 1
+        print("in the loop, time number {0}".format(loop_counter))
         f = open('currdate.txt','w')
         tmp = f.write(date.strftime('%Y-%m-%d'))
         f.close()
 
-        script = '''DATE={0}
-for HOUR in 0{{0..9}} {{10..23}}
+        for hour in range(24):
+            job='''#PBS -l nodes=1:ppn=1
+#PBS -l walltime=01:00:00
+#PBS -N geoParkScrape
+#PBS -j oe
+
+cd /users/a/r/areagan/scratch/geotweets
+export LD_LIBRARY_PATH=/users/a/r/areagan/usr/lib
+export PATH=/users/a/r/areagan/usr/lib:/users/a/r/areagan/usr/bin:/users/a/r/areagan/usr:$PATH
+
+. pyenv/bin/activate
+
+for MINUTE in 00 15 30 45
 do
-  sleep 2
-  echo "${{DATE}}-${{HOUR}}"
-  export DATE
-  export HOUR
-  qsub -qshortq -V runHour.qsub
+  echo "processing {0}-{1:02d}-${{MINUTE}}"
+  /usr/bin/time -v gzip -cd /users/a/r/areagan/scratch/geodb/{0}/{0}-{1:02d}-${{MINUTE}}.gz | python geofunctions.py "{0}"
+  echo "done"
 done
+echo "delete me"'''.format(date.strftime('%Y-%m-%d'),hour)
 
-\\rm {1}.sh
-
-'''.format(date.strftime('%Y-%m-%d'),ctime)
-
-        print 'writing {}.sh'.format(ctime)
-        f = open('{}.sh'.format(ctime),'w')
-        f.write(script)
-        f.close()
-    
-        qstatus = subprocess.check_output(". {}.sh".format(ctime),shell=True)
-        print qstatus
-
+            subprocess.call("echo '{0}' | qsub -qshortq".format(job),shell=True)
+            time.sleep(0.1)
+        
+        jobs_remaining -= 24
+        print("24 jobs submitted, {0} jobs remaining".format(jobs_remaining))
 
 
 
